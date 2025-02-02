24b5fb07d4336c280b947462150a602116eea720
riccardo.tornesello <riccardo.tornesello@gmail.com>
2025-02-02 23:39:53 +0100

    add footer

---
+++ qualify-results/src/components/footer.svelte
@@ -0,0 +1,6 @@
+<footer class="w-full p-5 text-center font-bold text-gray-200">
+	<p>
+		Made with ❤️ by
+		<a href="https://github.com/riccardotornesello" target="_blank" rel="noopener"> Riccardo Tornesello </a>
+	</p>
+</footer>
--- qualify-results/src/routes/+layout.svelte
+++ qualify-results/src/routes/+layout.svelte
@@ -2,11 +2,17 @@
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import '../app.css';
+	import Footer from '../components/footer.svelte';
+
 	let { children } = $props();
 </script>
 
 <ParaglideJS {i18n}>
-	<div class="dark min-h-screen bg-[#111a22] text-white">
-		{@render children()}
+	<div class="dark flex min-h-screen flex-col bg-[#111a22] text-white">
+		<main class="flex-1">
+			{@render children()}
+		</main>
+
+		<Footer />
 	</div>
 </ParaglideJS>
