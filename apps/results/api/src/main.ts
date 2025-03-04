df667d11f0f5b1538ca0f7a0e121258830d9f11a
riccardo.tornesello <riccardo.tornesello@gmail.com>
2025-03-04 23:50:17 +0100

    prepare nestjs for cloud function

--- apps/results/api/src/main.ts
+++ apps/results/api/src/main.ts
@@ -1,13 +1,36 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor } from '@nestjs/common';
+import { http } from '@google-cloud/functions-framework';
+import { Logger } from '@nestjs/common';
+import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+const server = express();
+
+async function bootstrap(expressInstance) {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
 
   // Serialization
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
-  await app.listen(process.env.PORT ?? 3000);
+  return app.init();
 }
-bootstrap();
+
+bootstrap(server).then(async (app) => {
+  // Start the server
+  if (process.env.ENVIRONMENT === 'production') {
+    Logger.log('🚀 Starting production server...');
+  } else {
+    const port = process.env.PORT || 3000;
+
+    Logger.log(`🚀 Starting development server on http://localhost:${port}`);
+
+    await app.listen(port);
+  }
+});
+
+http('apiNEST', server);
---
+++ apps/results/api/webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = (options, webpack) => {
+  const lazyImports = [
+    '@nestjs/microservices/microservices-module',
+    '@nestjs/websockets/socket-module',
+  ];
+
+  return {
+    ...options,
+    externals: ['@google-cloud/functions-framework'],
+    plugins: [
+      ...options.plugins,
+      new webpack.IgnorePlugin({
+        checkResource(resource) {
+          if (lazyImports.includes(resource)) {
+            try {
+              require.resolve(resource);
+            } catch (err) {
+              return true;
+            }
+          }
+          return false;
+        },
+      }),
+    ],
+    output: {
+      ...options.output,
+      library: {
+        ...options.output.library,
+        type: 'commonjs2',
+      },
+    },
+  };
+};
